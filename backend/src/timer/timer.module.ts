f2879455d1c81a796a412f27d9df998c0dbb059c
Mateusz Kijonka <mateuszkijonka@MacBook-Air-Mateusz.local>
2025-07-02 19:33:40 +0200

    fresh

--- backend/src/timer/timer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TimerController } from './timer.controller';
-import { TimerService } from './timer.service';
-
-@Module({
-  controllers: [TimerController],
-  providers: [TimerService],
-})
-export class TimerModule {}
